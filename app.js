b731077503c40c5b2b4a3bdc60da1b598197d2a3
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2020-08-27 19:54:41 +0100

    update app.js db url

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use("/admin", adminRoutes);
 
 
 
-var mongoDB = "mongodb+srv://iceconnected:39913991@cluster0-kjwnq.mongodb.net/bank3?retryWrites=true&w=majority";
+var mongoDB = "mongodb+srv://iceconnected:39913991@cluster0-kjwnq.mongodb.net/bank5?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connections;
 db.concat("error", console.error.bind(console, "MongoDB connection error."));
